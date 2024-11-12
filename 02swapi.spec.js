f02fec64e933d6eced9cd6ae2bd2c2ec486cc1c0
cauajanu-dev <cauadiscord886@gmail.com>
2024-11-12 10:20:06 -0300

    Update 02swapi.spec.js

--- 02swapi.spec.js
+++ 02swapi.spec.js
@@ -1,7 +1,5 @@
-const request = require('supertest');
-// Teste 2: Obter o segundo personagem
 test('should fetch the second person', async () => {
-    const resposta = await request('https://swapi.dev/api').get('/people/2/');
-    expect(resposta.status).toBe(200);
-    expect(resposta.body).toHaveProperty('name', 'C-3PO');
-  });
+  const resposta = await request('https://swapi.dev/api').get('/people/2/');
+  expect(resposta.status).toBe(200);
+  expect(resposta.body).toHaveProperty('name', 'C-3PO');
+}, 10000);  // 10 segundos para o teste, alterando o valor do tempo limite
